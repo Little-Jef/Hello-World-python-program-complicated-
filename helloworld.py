0e2550d5208ef414564ce36f747f57cd4995b797
Jeferson Santos <62681204+Little-Jef@users.noreply.github.com>
2021-04-08 18:58:28 -0400

    Add files via upload

---
+++ helloworld.py
@@ -0,0 +1,18 @@
+
+#creating a variable equals to zero
+i = 0 
+
+#defining letter for word variables
+h = 'Hello' 
+w = ' World!' 
+ 
+ #function hello
+def hello(): 
+	if i == 0: 
+		print(h+w) 
+	else: 
+		print(h+w)
+
+ 
+#Hello World!
+hello()		 
